949516dbb470065af6f920683abf7018a9f6ee97
= <=>
2020-03-11 22:59:52 +0545

    khoi ke khoi ke

--- db/schema.rb
+++ db/schema.rb
@@ -289,7 +289,6 @@ ActiveRecord::Schema.define(version: 2020_03_10_121211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "specification"
-    t.integer "item_classification"
     t.integer "item_category_id"
     t.integer "item_classification_no"
     t.integer "item_register_page_no"
