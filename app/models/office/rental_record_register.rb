dceb070dbfc55403289dc7ad3674c0e6d9c6f45c
= <=>
2020-04-15 13:31:31 +0545

    df

--- app/models/office/rental_record_register.rb
+++ app/models/office/rental_record_register.rb
@@ -1,5 +1,5 @@
 class Office::RentalRecordRegister < ApplicationRecord
   belongs_to :user
   belongs_to :office, class_name: "Office::Office"
-  has_many: :rental_record_register_items, class_name: "Office::RentalRecordRegisterItem"
+  has_many :rental_record_register_items, class_name: "Office::RentalRecordRegisterItem"
 end
