7b95a3bdc67d0d2463f6118088f0093f791359e4
= <=>
2020-04-15 13:29:58 +0545

    df

--- app/models/office/land_and_structure_record_book.rb
+++ app/models/office/land_and_structure_record_book.rb
@@ -1,5 +1,5 @@
 class Office::LandAndStructureRecordBook < ApplicationRecord
   belongs_to :user
   belongs_to :office, class_name: "Office::Office"
-  has_many :land_and_structure_record_book_items,  class_name: "Office::LandAndStructureRecordBookItem"
+  has_many :land_and_structure_record_book_items, class_name: "Office::LandAndStructureRecordBookItem"
 end
