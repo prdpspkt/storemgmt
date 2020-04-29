61bf3e4fbb24dc9d46e52241939137f5b42aa1bb
Pradeep Sapkota <prdpspkt@gmail.com>
2020-04-29 10:41:31 +0545

    fixed purchase order print no vat no amount issue

--- app/controllers/office/purchase_orders_controller.rb
+++ app/controllers/office/purchase_orders_controller.rb
@@ -111,6 +111,7 @@ class Office::PurchaseOrdersController < OfficeController
     @report_name = "खरिद आदेश"
     @old_form_no = 45
     @form_no = 402
+    binding.pry
     respond_to do |format|
       format.html
       format.pdf do
