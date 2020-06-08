f18e77c479892982534955346e0a2e3c7b7450bb
= <=>
2020-06-08 15:24:08 +0545

    done some changes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
-gem 'roo'
 
+gem 'roo'
 gem 'wicked_pdf'
 gem 'sidekiq'
 gem 'caxlsx_rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+// require material.min
 //= require activestorage
 //= require datatables
 //= require select2
