4afbba88aad43b66d79f613c8137611e71c40c94
= <=>
2020-07-11 20:56:24 +0545

    comp

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,10 @@
 $(document).on('turbolinks:load', function () {
-    $(".data-table").DataTable({
-        "pageLength" : 100
-    });
+   
+    if ( ! $.fn.DataTable.isDataTable( '.data-table' ) ) {
+        $(".data-table").DataTable({
+                "pageLength" : 100
+        });
+    }
     $('.select2').select2({
         theme: 'bootstrap'
     });
