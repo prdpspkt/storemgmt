c15d934f575b01ffa906dd2ec6f277f9b66a9a42
= <=>
2020-08-01 12:21:06 +0545

    hiello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
 
 
   protect_from_forgery
-  if Rails.env == "production"
-    rescue_from Exception do |exception|
-      flash[:error] = "#{exception_message exception}"
-      logger.debug("#{exception.class.to_s}: #{exception.message}")
-      redirect_to request.referrer || root_path
-    end
-  end
+  # if Rails.env == "production"
+  #   rescue_from Exception do |exception|
+  #     flash[:error] = "#{exception_message exception}"
+  #     logger.debug("#{exception.class.to_s}: #{exception.message}")
+  #     redirect_to request.referrer || root_path
+  #   end
+  # end
 
 
 
