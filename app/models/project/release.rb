7c3c1d4ecee04834f71356b42ee6c83afca6a304
= <=>
2020-08-02 08:00:29 +0545

    Check magfaram

--- app/models/project/release.rb
+++ app/models/project/release.rb
@@ -1,5 +1,5 @@
 class Project::Release < ApplicationRecord
-  has_many :release_items, class_name: "Project::ReleaseItem"
+  has_many :release_items, class_name: "Project::ReleaseItem", dependent: :destry
   belongs_to :office, class_name: "Office::Office"
   belongs_to :project, class_name: "Project::Project"
   belongs_to :user
