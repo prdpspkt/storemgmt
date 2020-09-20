685bcb9ee7bd7ad862f9e0d1ac5dfa5bb37d5eda
Pradeep Sapkota <prdpspkt@gmail.com>
2020-09-20 14:15:34 +0545

    Update purchase_entry.rb

--- app/models/project/purchase_entry.rb
+++ app/models/project/purchase_entry.rb
@@ -1,6 +1,6 @@
 class Project::PurchaseEntry < ApplicationRecord
   has_many :purchase_entry_items, class_name: "Project::PurchaseEntryItem", dependent: :delete_all
-  belongs_to :purchase_order, :class_name => 'Office::PurchaseOrder', optional: true
+  belongs_to :purchase_order, :class_name => 'Project::PurchaseOrder', optional: true
   belongs_to :tender, :class_name => 'Project::Tender', optional:  true
   belongs_to :store_body, :class_name => 'Office::StoreBody'
   belongs_to :office, :class_name => 'Office::Office'
